68c89f8c342a64485cd824693a3860a5fcd02304
Sebastian James <sabbadoo32@gmail.com>
2025-07-16 16:29:58 -0500

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,5 +1,18 @@
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb+srv://sebastianjames:d%402119ChartwellDrive@cluster0.gh4va.mongodb.net/EchoDrift?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
 export default async function handler(req, res) {
-  res.status(200).json({
-    MONGODB_URI: process.env.MONGODB_URI || "undefined"
-  });
+  try {
+    await client.connect();
+    const db = client.db('EchoDrift');
+    const collection = db.collection('Modules');
+    const data = await collection.find({}).toArray();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: 'MongoDB connection failed', detail: e.message });
+  } finally {
+    await client.close();
+  }
 }
