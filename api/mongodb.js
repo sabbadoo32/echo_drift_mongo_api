d098da66881ab686c6160ad363e058a431d55842
Sebastian James <sabbadoo32@gmail.com>
2025-07-16 16:36:58 -0500

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,18 +1,28 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
-const uri = "mongodb+srv://sebastianjames:d%402119ChartwellDrive@cluster0.gh4va.mongodb.net/EchoDrift?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+let cachedClient = null;
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
+  const uri = "mongodb+srv://sebastianjames:d%402119ChartwellDrive@cluster0.gh4va.mongodb.net/EchoDrift?retryWrites=true&w=majority&appName=Cluster0";
+
+  console.log("Connecting to MongoDB...");
+
   try {
-    await client.connect();
-    const db = client.db('EchoDrift');
-    const collection = db.collection('Modules');
+    if (!cachedClient) {
+      const client = new MongoClient(uri);
+      cachedClient = await client.connect();
+      console.log("New MongoDB connection established");
+    } else {
+      console.log("Reusing cached MongoDB connection");
+    }
+
+    const db = cachedClient.db("EchoDrift");
+    const collection = db.collection("Modules");
     const data = await collection.find({}).toArray();
+
     res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ error: 'MongoDB connection failed', detail: e.message });
-  } finally {
-    await client.close();
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    res.status(500).json({ error: "Internal Server Error", detail: error.message });
   }
-};
+}
