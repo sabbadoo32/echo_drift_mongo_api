9538baf0354b18fe3bb92058da3fec41a9985810
Sebastian James <sabbadoo32@gmail.com>
2025-07-16 16:56:59 -0500

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,16 +1,21 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
+console.log("Using URI:", uri);
+
 const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
   try {
+    console.log("Connecting to Mongo...");
     await client.connect();
+    console.log("Connected");
 
     const db = client.db("EchoDrift");
     const collection = db.collection("Modules");
     const data = await collection.find({}).toArray();
 
+    console.log("Fetched data:", data);
     res.status(200).json(data);
   } catch (error) {
     console.error("MongoDB error:", error);
