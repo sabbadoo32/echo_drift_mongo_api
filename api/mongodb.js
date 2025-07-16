46b2230c717665de9f872e51c4726acedc21f79f
Sebastian James <sabbadoo32@gmail.com>
2025-07-16 16:32:58 -0500

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,9 +1,9 @@
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://sebastianjames:d%402119ChartwellDrive@cluster0.gh4va.mongodb.net/EchoDrift?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
     await client.connect();
     const db = client.db('EchoDrift');
@@ -15,4 +15,4 @@ export default async function handler(req, res) {
   } finally {
     await client.close();
   }
-}
+};
